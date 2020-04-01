7726eff244351090e5d2a128e329a8dce7eec711
cjgc <chjugomez@utp.edu.co>
2020-03-31 19:11:15 -0500

    update and delete methods were added

--- homework2/src/main/java/co/edu/utp/isc/gia/restuser/dto/UserController.java
+++ homework2/src/main/java/co/edu/utp/isc/gia/restuser/dto/UserController.java
@@ -7,13 +7,17 @@ package co.edu.utp.isc.gia.restuser.dto;
 
 import co.edu.utp.isc.gia.restuser.service.UserService;
 import co.edu.utp.isc.gia.restuser.web.UserDto;
-import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,9 +35,11 @@ public class UserController {
     }
     
     @PostMapping() // POST http://localhost:8080/user
-    public UserDto save(@RequestBody UserDto user) {
-
-        return userService.save(user);
+    public ResponseEntity<String> save(@RequestBody UserDto user) {
+        if (user == null) return new ResponseEntity<> ( HttpStatus.BAD_REQUEST);
+        
+        userService.save(user);
+        return new ResponseEntity<> ( HttpStatus.OK);
     }
     
     @GetMapping
@@ -42,9 +48,28 @@ public class UserController {
     }
     
     @GetMapping("/{id}")
-    public UserDto findOne(
+    @ResponseBody
+    public ResponseEntity<String> findOne(
             @PathVariable("id") Long id
     ) {
-        return userService.findOne(id);
+        UserDto user = userService.findOne(id);
+        if (user == null) return new ResponseEntity<> ( HttpStatus.NOT_FOUND);
+        return new ResponseEntity(user, HttpStatus.OK);
     }
+    
+    @PutMapping
+    public ResponseEntity<String> update(
+            @RequestBody UserDto user) {
+        //if (userService.update(user) == null) 
+        //    return new ResponseEntity<> (HttpStatus.BAD_REQUEST);
+        
+        return new ResponseEntity<> (HttpStatus.OK);
+    }
+    
+    @DeleteMapping("/{id}")
+    public UserDto delete(
+            @PathVariable("id") Long id) {
+        return null;
+    }
+    
 }
