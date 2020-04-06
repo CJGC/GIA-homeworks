5e5c95f6583d46b1f2ef8f0e6471ccf72077ee93
cjgc <chjugomez@utp.edu.co>
2020-04-06 11:37:25 -0500

    Professor entity

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/data/entity/Professor.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ *
+ * @author CJ
+ */
+
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter
+@Table(name = "Usuario")
+@Entity()
+public class Professor implements Serializable  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String name;
+    private String email;    
+}
