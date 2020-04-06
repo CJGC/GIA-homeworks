0dfe0152ba2bc23065043b1282cefd9553d544b7
cjgc <chjugomez@utp.edu.co>
2020-04-06 12:01:45 -0500

    Application config

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package co.edu.utp.isc.gia.examsapp.config;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CJ
+ */
+@Configuration
+public class ApplicationConfig {
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
