68dd903ce363ccf0f98c97123bac44b18ffd1b9e
cjgc <chjugomez@utp.edu.co>
2020-04-07 18:21:31 -0500

    primeng inputtext and primeng button modules were introduced

--- homework6/src/app/app.module.ts
+++ homework6/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 
 import { AppComponent } from './app.component';
 import { UserFormularyComponent } from './user-formulary/user-formulary.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { UserFormularyComponent } from './user-formulary/user-formulary.componen
     UserFormularyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
