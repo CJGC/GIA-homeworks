90554a34c882e34be346ee84ee0338540c177c4d
cjgc <chjugomez@utp.edu.co>
2020-04-07 16:58:58 -0500

    userFormulary component

---
+++ homework6/src/app/user-formulary/user-formulary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hom-user-formulary',
+  templateUrl: './user-formulary.component.html',
+  styleUrls: ['./user-formulary.component.css']
+})
+export class UserFormularyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
