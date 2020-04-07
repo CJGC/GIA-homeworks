26f0507aa823864d78a56e721b87d77318a8f28e
cjgc <chjugomez@utp.edu.co>
2020-04-07 18:27:08 -0500

    Logic to save user was coded

--- homework6/src/app/user-formulary/user-formulary.component.ts
+++ homework6/src/app/user-formulary/user-formulary.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../dto/user';
+import {InputTextModule} from 'primeng/inputtext';
 
 @Component({
   selector: 'hom-user-formulary',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormularyComponent implements OnInit {
 
+  public userDto : User;
+  public users : Array<User>;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.userDto = new User();
+    this.users = new Array<User>();
   }
 
+  ngOnChange() {
+    console.log("Changed");
+  }
+
+  public saveUser() : void {
+    this.users.push(this.userDto);
+    this.userDto = new User();
+  }
+  
 }
