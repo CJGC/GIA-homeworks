464957f10797842528233c4b390abd88d11c0f6d
cjgc <chjugomez@utp.edu.co>
2020-04-07 21:36:32 -0500

    inconsistencies in update process fixed

--- homework6/src/app/user-formulary/user-formulary.component.ts
+++ homework6/src/app/user-formulary/user-formulary.component.ts
@@ -10,9 +10,13 @@ import {InputTextModule} from 'primeng/inputtext';
 export class UserFormularyComponent implements OnInit {
 
   public userDto : User;
+  public userDtoCopy : User;
   public users : Array<User>;
+  public showAddBtn : boolean;
 
-  constructor() { }
+  constructor() {
+    this.showAddBtn = true;
+  }
 
   ngOnInit(): void {
     this.userDto = new User();
@@ -24,8 +28,38 @@ export class UserFormularyComponent implements OnInit {
   }
 
   public saveUser() : void {
+    if(this.users.indexOf(this.userDto) != -1) 
+      return;
     this.users.push(this.userDto);
-    this.userDto = new User();
+    this.resetUser();
   }
   
+  public resetUser() : void {
+    this.userDto = new User();
+    this.showAddBtn = true;
+  }
+
+  private makeCopy() : void {
+    this.userDtoCopy = new User();
+    this.userDtoCopy.setId(this.userDto.getId());
+    this.userDtoCopy.setUsername(this.userDto.getUsername());
+    this.userDtoCopy.setName(this.userDto.getName());
+    this.userDtoCopy.setEmail(this.userDto.getEmail());
+    this.userDtoCopy.setPassword(this.userDto.getPassword());
+  }
+
+  public receiveEvent($event) : void {
+    this.showAddBtn = false;
+    this.userDto = $event;
+    this.makeCopy();
+  }
+
+  public cancelUpdate() : void {
+    this.userDto.setId(this.userDtoCopy.getId());
+    this.userDto.setUsername(this.userDtoCopy.getUsername());
+    this.userDto.setName(this.userDtoCopy.getName());
+    this.userDto.setEmail(this.userDtoCopy.getEmail());
+    this.userDto.setPassword(this.userDtoCopy.getPassword());
+    this.resetUser();
+  }
 }
