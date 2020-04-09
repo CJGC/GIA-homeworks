6ceef80ad3314b6f0946a34fe4ff41e1fe5ae70e
cjgc <chjugomez@utp.edu.co>
2020-04-08 19:27:53 -0500

    Crossorigin injection

--- homework4/src/main/java/co/edu/utp/isc/gia/restuser/web/controller/UserController.java
+++ homework4/src/main/java/co/edu/utp/isc/gia/restuser/web/controller/UserController.java
@@ -10,6 +10,7 @@ import co.edu.utp.isc.gia.restuser.web.dto.UserDto;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,9 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("user")
+@CrossOrigin(origins="*")
 public class UserController {
     
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
