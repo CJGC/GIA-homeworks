3d84a07841502a2e174ab9d889894bfbc728034c
cjgc <chjugomez@utp.edu.co>
2020-04-12 11:29:21 -0500

    Exam entity was implemented

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/data/entity/Exam.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ *
+ * @author CJ
+ */
+
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter
+@Table(name = "Professor")
+@Entity()
+public class Professor implements Serializable  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String identificationCard;
+    private String name;
+    private String lastname;
+    private String email;
+    private String username;
+    private String password;
+}
