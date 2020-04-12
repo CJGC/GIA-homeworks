0604f13e64b2a117d7a76a98d779281f7954dcc8
cjgc <chjugomez@utp.edu.co>
2020-04-12 15:16:32 -0500

    abstract question class

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/web/dto/abstractdto/QuestionDto.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.web.dto.abstractdto;
+
+import java.io.File;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author CJ
+ */
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter
+public abstract class QuestionDto {
+    
+    private String questionType;
+    private String description;
+    private File image;
+    private double weight;
+    
+}
