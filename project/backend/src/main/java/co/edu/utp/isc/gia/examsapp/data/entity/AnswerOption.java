9cea40de451474eac11357351f58ee8075a1be84
cjgc <chjugomez@utp.edu.co>
2020-04-12 16:18:25 -0500

    AnswerOption entity

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/data/entity/AnswerOption.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ *
+ * @author CJ
+ */
+
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter
+@Table(name = "Answer_option")
+@Entity()
+public class AnswerOption implements Serializable  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String index;
+    private String description;
+    private Boolean correctAnswer;
+    
+    @Column(precision=16, scale=2)
+    private Double weight;
+    
+    @ManyToOne(fetch = FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn
+    private Question question;
+    
+}
