0a8c751ce7de4c11b4d6523697a6a4f4d79d8039
cjgc <chjugomez@utp.edu.co>
2020-04-12 09:58:40 -0500

    Exam table was created

--- project/backend/src/main/resources/db/migration/V0.1__Initial_Version.sql
+++ project/backend/src/main/resources/db/migration/V0.1__Initial_Version.sql
@@ -8,5 +8,20 @@ CREATE TABLE Professor (
     username varchar(100) not null,
     password varchar(100) not null,
 
-    CONSTRAINT Usuario_pk PRIMARY KEY (id)
+    CONSTRAINT pk_professor PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Exam (
+    id bigint auto_increment,
+    name varchar(100) not null,
+    link varchar(512) not null,
+    max_grade double default 0.0,
+    description varchar(1024) not null,
+    examtime int default 0,
+    professor_id bigint,
+
+    constraint pk_exam primary key (id),
+    constraint fk_professor foreign key (professor_id)
+    references Professor(id)
 );
