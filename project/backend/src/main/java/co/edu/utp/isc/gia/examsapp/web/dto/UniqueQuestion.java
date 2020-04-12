cc631731c7d1f830ff3234a3bdf03593215ea168
cjgc <chjugomez@utp.edu.co>
2020-04-12 15:15:28 -0500

    Uniquestion class

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/web/dto/UniqueQuestion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.web.dto;
+
+import co.edu.utp.isc.gia.examsapp.web.dto.abstractdto.QuestionDto;
+
+/**
+ *
+ * @author CJ
+ */
+public class UniqueQuestion extends QuestionDto {
+    
+}
