9eb76e2a29f2e37ba0cdc552bb4db263de8f3b5e
cjgc <chjugomez@utp.edu.co>
2020-04-13 15:35:29 -0500

    Open response Dto

---
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/web/dto/OpenResponseDto.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.examsapp.web.dto;
+
+import co.edu.utp.isc.gia.examsapp.data.entity.ExamStudent;
+import co.edu.utp.isc.gia.examsapp.data.entity.Question;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author CJ
+ */
+@AllArgsConstructor @NoArgsConstructor
+@Setter @Getter
+public class OpenResponseDto implements Serializable {
+    
+    private Long id;
+    private String content;
+    private Double valoration;
+    private ExamStudent examStudent;
+    private Question question;
+}
