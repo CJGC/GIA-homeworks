a3427408b16aa079c8a1160f9b56530b2abd835e
cjgc <chjugomez@utp.edu.co>
2020-04-14 06:59:26 -0500

    New attributes validators were added

--- project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/ExamStudentValidator.java
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/ExamStudentValidator.java
@@ -7,7 +7,6 @@ package co.edu.utp.isc.gia.examsapp.validators;
 
 
 import co.edu.utp.isc.gia.examsapp.web.dto.ExamStudentDto;
-import java.util.regex.Pattern;
 
 /**
  *
@@ -32,15 +31,36 @@ public class ExamStudentValidator {
     public void validateId() throws Exception {
         if (this.examStudent.getId() == null)
             throw new Exception("examStudent id is null");
+    }
+    
+    public void validateDefinitiveGrade() throws Exception {
+        if (this.examStudent.getDefinitive_grade() == null)
+            throw new Exception("examStudent definitive grade is null");
+    }
+
+    public void validateStudent() throws Exception {
+        if (this.examStudent.getStudent() == null)
+            throw new Exception("examStudent student is null");
+    }
+    
+    public void validateExam() throws Exception {
+        if (this.examStudent.getExam() == null)
+            throw new Exception("examStudent exam is null");
     }    
     
     public void performValidationsExcept(String attribute) throws Exception {
         this.isNull();
         if (!attribute.equals("id")) this.validateId();
+        if (!attribute.equals("definitiveGrade")) this.validateDefinitiveGrade();
+        if (!attribute.equals("student")) this.validateStudent();
+        if (!attribute.equals("exam")) this.validateExam();
     }
     
     public void performValidations() throws Exception {
         this.isNull();
         this.validateId();
+        this.validateDefinitiveGrade();
+        this.validateStudent();
+        this.validateExam();
     }
 }
