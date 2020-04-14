4956a9d5b367f7c9eebd7ae40b3de8230375351f
cjgc <chjugomez@utp.edu.co>
2020-04-13 19:06:42 -0500

    validators for new answerOption attributes were implemented

--- project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/AnswerOptionValidator.java
+++ project/backend/src/main/java/co/edu/utp/isc/gia/examsapp/validators/AnswerOptionValidator.java
@@ -6,8 +6,12 @@
 package co.edu.utp.isc.gia.examsapp.validators;
 
 
+import co.edu.utp.isc.gia.examsapp.data.entity.SelectedResponse;
 import co.edu.utp.isc.gia.examsapp.web.dto.AnswerOptionDto;
+import co.edu.utp.isc.gia.examsapp.web.dto.SelectedResponseDto;
+import co.edu.utp.isc.gia.examsapp.web.dto.abstractdto.QuestionDto;
 import java.util.regex.Pattern;
+import org.modelmapper.ModelMapper;
 
 /**
  *
@@ -15,37 +19,72 @@ import java.util.regex.Pattern;
  */
 public class AnswerOptionValidator {
     private AnswerOptionDto answerOption;
+    private final SelectedResponseValidator selecRespValidator;
+    private final QuestionValidator questionValidator;
+    private final ModelMapper modelMapper;
+    
+    public AnswerOptionValidator(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+        this.selecRespValidator = new SelectedResponseValidator();
+        this.questionValidator = new QuestionValidator();
+    }
 
-    public AnswerOptionDto getansweroption() {
+    public AnswerOptionDto getAnswerOption() {
         return answerOption;
     }
 
-    public void setansweroption(AnswerOptionDto answerOption) {
+    public void setAnswerOption(AnswerOptionDto answerOption) {
         this.answerOption = answerOption;
     }
     
     public void isNull() throws Exception {
         if (this.answerOption == null)
-            throw new Exception("answer option object is null");
+            throw new Exception("AnswerOption object is null");
      }
     
     public void validateId() throws Exception {
         if (this.answerOption.getId() == null)
-            throw new Exception("answer option id is null");
+            throw new Exception("AnswerOption id is null");
     }
 
     public void validateIndex() throws Exception {
         if (this.answerOption.getIndex() == null)
-            throw new Exception("answer option index is null");
+            throw new Exception("AnswerOption index is null");
         if (Pattern.matches("", this.answerOption.getIndex()))
-            throw new Exception ("answer option index is empty");
+            throw new Exception ("AnswerOption index is empty");
     }
     
     public void validateDescription() throws Exception {
         if (this.answerOption.getDescription() == null)
-            throw new Exception("answer option description is null");
+            throw new Exception("AnswerOption description is null");
         if (Pattern.matches("", this.answerOption.getDescription()))
-            throw new Exception ("anwer option description is empty");
+            throw new Exception ("AnswerOption description is empty");
+    }
+    
+    public void validateCorrectAnswer() throws Exception {
+        if (this.answerOption.getCorrectAnswer() == null)
+            throw new Exception("AnswerOption correctAnswer is null");
+    }
+    
+    public void validateWeight() throws Exception {
+        if (this.answerOption.getWeight() == null)
+            throw new Exception("AnswerOption is null");
+    }
+    
+    public void validateSelectedResponses() throws Exception {
+        
+        for (SelectedResponse sr : this.answerOption.getSelectedResponses()) {
+            selecRespValidator.setselectedResponse(this.modelMapper.map(sr, 
+                    SelectedResponseDto.class));
+            selecRespValidator.performValidationsExcept("id");
+        }
+    }
+
+    public void validateQuestion() throws Exception {
+        if (this.answerOption.getQuestion() == null)
+            throw new Exception("AnswerOption question is null");
+        this.questionValidator.setquestion(this.modelMapper.map(
+        this.answerOption.getQuestion(), QuestionDto.class));
     }
     
     public void performValidationsExcept(String attribute) throws Exception {
@@ -53,12 +92,19 @@ public class AnswerOptionValidator {
         if (!attribute.equals("id")) this.validateId();
         if (!attribute.equals("index")) this.validateIndex();
         if (!attribute.equals("description")) this.validateDescription();
+        if (!attribute.equals("correctAnswer")) this.validateCorrectAnswer();
+        if (!attribute.equals("weight")) this.validateWeight();
+        if (!attribute.equals("selectedResponses")) this.validateSelectedResponses();
+        if (!attribute.equals("question")) this.validateQuestion();
     }
     
     public void performValidations() throws Exception {
-        this.isNull();
         this.validateId();
         this.validateIndex();
         this.validateDescription();
+        this.validateCorrectAnswer();
+        this.validateWeight();
+        this.validateSelectedResponses();
+        this.validateQuestion();
     }
 }
