5cf53e5a80acf94b957e392caf41796c05083720
cjgc <chjugomez@utp.edu.co>
2020-04-15 14:07:47 -0500

    Class service name was modified

--- project/frontend/src/app/services/Professsor.service.ts
+++ project/frontend/src/app/services/Professsor.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class ProfessorDtoService {
+export class ProfessorService {
 
   constructor(private http : HttpClient) { }
 
