9c816460ce495e3475c19057132b4bb8ca87f6d7
cjgc <chjugomez@utp.edu.co>
2020-04-15 13:47:41 -0500

    OpenQuestion Dto

---
+++ project/frontend/src/app/dto/OpenQuestionDto.ts
@@ -0,0 +1,9 @@
+import {OnInit } from '@angular/core';
+import { QuestionDto } from './abstractDto/QuestionDto';
+import { ExamStudentDto } from './ExamStudentDto';
+
+export class OpenQuestionDto extends QuestionDto implements OnInit {
+    
+    ngOnInit() {  }
+
+}
