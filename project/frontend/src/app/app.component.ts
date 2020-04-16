e82f449514751c5d275f21522214a9546b9e8b43
cjgc <chjugomez@utp.edu.co>
2020-04-16 00:15:44 -0500

    Professor global instance service was added

--- project/frontend/src/app/app.component.ts
+++ project/frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfessorGlobalInstance } from './services/ProfessorGlobalInstance.service';
 
 @Component({
   selector: 'hom-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProfessorGlobalInstance]
 })
 export class AppComponent {
   title = 'Main';
