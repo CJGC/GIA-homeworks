ca97a88b131d7c82eb2e4611fd04084c32eb7b97
cjgc <chjugomez@utp.edu.co>
2020-04-16 00:02:44 -0500

    create-professor and professor-main-view routes were added

--- project/frontend/src/app-routing.module.ts
+++ project/frontend/src/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './app/components/login/login.component';
-import { ProfessorFormularyComponent } from './app/professor-formulary/professor-formulary.component';
+import { ProfessorFormularyComponent } from './app/components/professor-formulary/professor-formulary.component';
+import { ProfessorMainViewComponent } from './app/components/professor-main-view/professor-main-view.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component : LoginComponent },
-  { path: 'create-professor', component: ProfessorFormularyComponent }
+  { path: 'create-professor', component: ProfessorFormularyComponent },
+  { path: 'professor-main-view', component: ProfessorMainViewComponent }
 ];
 
 @NgModule({
