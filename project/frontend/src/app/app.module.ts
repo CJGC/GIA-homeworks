689fa8091b026b7fce9e9dea911d4c1cc6e93729
cjgc <chjugomez@utp.edu.co>
2020-04-16 19:39:20 -0500

    MessagesModule, AppRoutingModule, EditrModule, SpinnerModule and ListboxModule were introduced

--- project/frontend/src/app/app.module.ts
+++ project/frontend/src/app/app.module.ts
@@ -1,35 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {TableModule} from 'primeng/table';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {HttpClientModule} from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
 import { ProfessorFormularyComponent } from './components/professor-formulary/professor-formulary.component';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserTableComponent } from './user-table/user-table.component';
+import { ExamTableComponent } from './components/exams-table/exam-table.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from '../app-routing.module';
 import { ProfessorMainViewComponent } from './components/professor-main-view/professor-main-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessagesModule } from 'primeng/messages';
+import { EditorModule } from 'primeng/editor';
+import { SpinnerModule } from 'primeng/spinner';
+import { ListboxModule } from 'primeng/listbox';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfessorFormularyComponent,
-    UserTableComponent,
+    ExamTableComponent,
     LoginComponent,
     ProfessorMainViewComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     InputTextModule,
     ButtonModule,
     TableModule,
     FormsModule,
     HttpClientModule,
-    FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    MessagesModule,
+    AppRoutingModule,
+    EditorModule,
+    SpinnerModule,
+    ListboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
