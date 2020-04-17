f4566b033c2d91873f3688f66b68f9fcd01f2d58
cjgc <chjugomez@utp.edu.co>
2020-04-16 19:30:54 -0500

    Professor global instance was removed

--- project/frontend/src/app/app.component.ts
+++ project/frontend/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ProfessorGlobalInstance } from './services/ProfessorGlobalInstance.service';
 
 @Component({
   selector: 'hom-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ProfessorGlobalInstance]
 })
 export class AppComponent {
   title = 'Main';
